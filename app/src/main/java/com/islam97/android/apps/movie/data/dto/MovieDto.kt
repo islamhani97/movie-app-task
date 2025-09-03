35dbe242403b19498c02fb2e70e04a5bb5a81ec8
Islam Hani <72826869+islamhani97@users.noreply.github.com>
2025-09-03 07:27:03 +0300

    Fix date parsing issue (#6)

--- app/src/main/java/com/islam97/android/apps/movie/data/dto/MovieDto.kt
+++ app/src/main/java/com/islam97/android/apps/movie/data/dto/MovieDto.kt
@@ -1,14 +1,13 @@
 package com.islam97.android.apps.movie.data.dto
 
 import com.google.gson.annotations.SerializedName
-import java.util.Date
 
 data class MovieDto(
     @SerializedName("id") val id: Int,
     @SerializedName("title") val title: String?,
     @SerializedName("poster_path") val posterPath: String?,
     @SerializedName("backdrop_path") val backdropPath: String?,
-    @SerializedName("release_date") val releaseDate: Date?,
+    @SerializedName("release_date") val releaseDate: String?,
     @SerializedName("overview") val overview: String?,
     @SerializedName("vote_average") val voteAverage: Double?,
     @SerializedName("genres") val genres: List<GenreDto>?,
